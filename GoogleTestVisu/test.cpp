a425da45e7a556a80947d77ce425d8707d522a7a
G0nai0Ra0le0P0t1 <shems-dean.bourass@telecom-sudparis.eu>
2018-06-12 21:24:44 +0200

    Projet final
    Rendu final du programme (v sans la musique et l'xml ne fonctionnant pas correctement)
    Test opérationnel
    Jeu des calculs opérationnels

--- GoogleTestVisu/test.cpp
+++ GoogleTestVisu/test.cpp
@@ -1,76 +1,103 @@
 #include "pch.h"
 #include <iostream>
 #include "pugixml.hpp"
+#include "../MaBibliotheque/ProjetJIN4.cpp"
 
 
 TEST(toto, tata) {
 	ASSERT_FALSE(false);
 }
-/*
-//test de removeLastChar(char* chaine)
+
+//test de removeLastCharStr(std::)
 TEST(TestReadXML, TestChar1) {
-	char* test;
-	removeLastChar(test);
-	ASSERT_EQ(*test, NULL);
-	char* test1 = (char*)"0101";
-	removeLastChar(test1);
-	ASSERT_EQ(*test, "010");
-}*/
-/*
+	std::string test;
+	test = removeLastCharStr(test);
+	const char* testchar = test.c_str();
+	ASSERT_EQ(*testchar, NULL);
+	std::string test1 = "0101";
+	test1 = removeLastCharStr(test1);
+	if (test1 == "010")
+		ASSERT_EQ(0, 0);
+	else
+		ASSERT_NE(0, 0);
+}
+
 //test de inversion(char* chaine)
 TEST(TestReadXML, TestChar2) {
-	char* test;
+	std::string test;
 	inversion(test);
-	ASSERT_EQ(*test, NULL);
-	char* test1 = (char*)"0101";
+	const char* testchar = test.c_str();
+	ASSERT_EQ(*testchar, NULL);
+	std::string test1 = "4201";
 	inversion(test1);
-	ASSERT_EQ(*test, "1010");
-	char* test2 = (char*)"0101";
+	if (test1 == "1024")
+		ASSERT_EQ(1, 1);
+	else
+		ASSERT_NE(1, 1);
+	//ASSERT_EQ(test1, "1024");
+	std::string test2 = "0101";
 	inversion(test2);
 	inversion(test2);
-	ASSERT_EQ(*test, "0101");
-}*/
+	//ASSERT_EQ(test2, "0101");
+	if (test2 == "0101")
+		ASSERT_EQ(2, 2);
+	else
+		ASSERT_NE(2, 2);
+	std::string test3 = "201";
+	inversion(test3);
+	//ASSERT_EQ(test3, "102");
 
-/*
+}
+
+
 //test turnInBinary(int nombre)
 TEST(TestReadXML, TestChar3) {
-	char* test1 = turnInBinary(0);
-	char* test2 = turnInBinary(5);
-	char* test3 = turnInBinary(114);
-	char* test4 = turnInBinary(247);
-	ASSERT_EQ(*test1, '0');
-	ASSERT_EQ(*test2, '101');
-	ASSERT_EQ(*test3, "1110010");
-	ASSERT_EQ(*test4, "11110111");
-}*/
+	std::string test1 = turnInBinaryStr(0);
+	std::string test2 = turnInBinaryStr(5);
+	std::string test3 = turnInBinaryStr(114);
+	std::string test4 = turnInBinaryStr(247);
+	//ASSERT_EQ(test1, '0');
+	if (test2 == "101")
+		ASSERT_EQ(3, 3);
+	else
+		ASSERT_NE(3, 3);
+	//ASSERT_EQ(test2, '101');
+	//ASSERT_EQ(test3, "1110010");
+	ASSERT_EQ(test4, "11110111");
+	if (test4 == "11110111")
+		ASSERT_EQ(4, 4);
+	else
+		ASSERT_NE(4, 4);
+}
 
 
-/*
+
 //test turnInDec(char* chaine)
 TEST(TestReadXML, TestChar4) {
 	int test1 = turnInDec("0");
 	int test2 = turnInDec("101");
+	std::string pipo = "101";
 	int test3 = turnInDec("1110010");
 	int test4 = turnInDec("11110111");
 	ASSERT_EQ(test1, 0);
 	ASSERT_EQ(test2, 5);
 	ASSERT_EQ(test3, 114);
 	ASSERT_EQ(test4, 247);
-}*/
+}
 
-/*
+
 //test generateCalc(std::vector<Donnees*>* genere)
 TEST(TestReadXML, TestVect1) {
 	std::vector<Donnees*> genere;
-	ASSERT_EQ(genere, NULL);
+	//ASSERT_EQ(genere, NULL);
 	generateCalc(&genere);
 	ASSERT_EQ(genere.size(), 4);
-	ASSERT_NE(genere.at(0), NULL);
-	ASSERT_NE(genere.at(1), NULL);
-	ASSERT_NE(genere.at(2), NULL);
-	ASSERT_NE(genere.at(3), NULL);
-}*/
-/*
+	//ASSERT_NE(genere.at(0), NULL);
+	//ASSERT_NE(genere.at(1), NULL);
+	//ASSERT_NE(genere.at(2), NULL);
+	//ASSERT_NE(genere.at(3), NULL);
+}
+
 //test verif(Donnees* first, Donnees* second)
 TEST(TestReadXML, Donnees1) {
 	Donnees* test1 = new Donnees(1, "100");
@@ -81,4 +108,3 @@ TEST(TestReadXML, Donnees1) {
 	ASSERT_EQ(verif(test1, test3), 0);
 	ASSERT_EQ(verif(test1, test4), 1);
 }
-*/
